6a1206c8153fea87e8f14f04c7699e78f261fd14
Tarun Reddy <132771817+Tarun6062@users.noreply.github.com>
2024-02-15 00:20:06 +0530

    Update and rename index.py to Var & Data Types.py

---
+++ Var & Data Types.py
@@ -0,0 +1,26 @@
+name = "tarun"   # String (tarun)
+age = 22         # Int    (22)
+price = 22.9     # Float  (22.9)
+print(age)       # Function (print)
+
+
+# Expression Excution 
+# Rule-1 string & Numeric can operate together with *
+
+A , B = 2 , 3
+txt = "@"
+print(2*txt*3)
+
+# Rule-2 FOR string and string can be operated with + operator
+
+C,D = "2",3
+TXT2 = "@"
+print((C + txt)* 3)
+
+# Rule-3 String & numeric value can operate together with * operator 
+
+
+  
+
+
+
