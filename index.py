df8d808b381e25cb7dd3b349b9b063132bca03ad
Tarun Reddy <132771817+Tarun6062@users.noreply.github.com>
2024-02-15 00:15:02 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,17 @@
+name = "tarun"   # String (tarun)
+age = 22         # Int    (22)
+price = 22.9     # Float  (22.9)
+print(age)       # Function (print)
+
+
+# Expression Excution
+
+A , B = 2 , 3
+txt = "@"
+print(2*txt*3)
+
+  # FOR string and string
+
+C,D = "2",3
+TXT2 = "@"
+print((C + txt)* 3)
