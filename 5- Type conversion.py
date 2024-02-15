0a42c0cb3db7a5fba19c8327ce7ef8f187bab522
Tarun Reddy <132771817+Tarun6062@users.noreply.github.com>
2024-02-16 00:51:25 +0530

    5- Type conversion.py

--- 5- Type conversion.py
+++ 5- Type conversion.py
@@ -15,3 +15,28 @@ a, b = 1, "5"
 c = int(b)    # converting string to int
 sum = a + c
 print(sum)
+
+# Practice Qestion 1
+A = int(input("a = "))
+B = int(input("b = "))
+
+print(A + B)
+
+# Practice question 2
+
+side = int(input("Enter the length"))
+area = side * side
+print(area)
+
+# Practice Question 3
+
+f1 = float(input("enter the first val :"))
+f2 = float(input("enter the second val :"))
+avg = (f1 + f2)/2
+print (avg)
+
+# Practice question 4
+
+p = int(input("enter the first val"))
+q = int(input("enter the second val"))
+print(p >= q)
