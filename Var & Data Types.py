b5fbbc4428919028203c5d81042932f0ba621f88
Tarun Reddy <132771817+Tarun6062@users.noreply.github.com>
2024-02-16 00:50:07 +0530

    Update Var & Data Types.py

--- Var & Data Types.py
+++ Var & Data Types.py
@@ -2,16 +2,18 @@ name = "tarun"   # String (tarun)
 age = 22         # Int    (22)
 price = 22.9     # Float  (22.9)
 print(age)       # Function (print)
+                 # OUTPUT - 22
 
+# Expression Excution
 
-# Expression Excution 
-# Rule-1 string & Numeric can operate together with *
+# Rule-1 String & Numeric value can operate with *
 
 A , B = 2 , 3
 txt = "@"
-print(2*txt*3)
+print(2*txt*3)   
+# output - @@@@@@
 
-# Rule-2 FOR string and string can be operated with + operator
+# Rule-2 FOR string and string  (CONCATINATION - if one string adds with another string with + operator)
 
 C,D = "2",3
 TXT2 = "@"
@@ -22,7 +24,48 @@ print((C + txt)* 3)
 a,b = 2,3
 c = 4
 print(a+c*b)
-  
+
+# Rule-4 arthemetic operator with the float value and int value will give output of float
+
+d = 5.0
+e = 2
+print(d+e)
+
+# Rule-5 Result of divisor operator with two integer will float
+
+m,k = 2,3
+p = m/k
+print(p)
+
+# Rule-6 Inter division with float and int will give int displayed as float
+
+o,l = 1.5,3
+i= o//l
+print(i, o/l)
+
+
+# floor it gives the closest int - Ex:- if we give 0.12 the output will be 0
+# floor gives closest integer, which is less than or equal to the float value
+# result of (o//l) is same as floor(o/l) 
+
+O,L = -12,5
+I = O//L
+print(I)
+
+# Rule-6 Reminder is negetive when the denominator is negative'
+
+# n-->  + - + -
+# d-->  + - - +
+# r-->  + + - +
 
 
 
+
+q,w = 5,-2
+E= q%w
+print(E)
+
+
+""" For multi-line 
+     comment """ 
+     
