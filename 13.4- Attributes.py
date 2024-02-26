ba8f0844914919edcbec0ae4286147ba19d571bc
Tarun Reddy <132771817+Tarun6062@users.noreply.github.com>
2024-02-26 23:03:40 +0530

    Add files via upload

---
+++ 13.4- Attributes.py
@@ -0,0 +1,21 @@
+
+
+# Class Attributes
+
+class Collage:
+    Collage_name = "YOO"
+    name = "nothing"         # Class attribute
+    
+    def __init__(self, name, age):
+        self.name = name     # object attribute >  Class attribute
+        self.age = age
+        print("your name is", self.name ," and your age is " ,self.age)
+        
+        
+s1 = Collage("kume", 20)
+s2 = Collage("tarun" , 24)
+s3 = Collage("midhun", 99)
+
+print(s1.Collage_name)
+print(s2.Collage_name)
+print(s3.Collage_name)
