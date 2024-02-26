6b88be6f346c591cc0ae6978e60347d0136c4455
Tarun Reddy <132771817+Tarun6062@users.noreply.github.com>
2024-02-26 23:03:04 +0530

    Add files via upload

---
+++ 13.2-class&obj.py
@@ -0,0 +1,19 @@
+class Person:
+    name  = "REDDY"
+    occup = "Developer"
+    age   = 22
+    def info(self):     # Self  parameter is a reference to the current instance of the class, and it is used to access variables that belongs to the class
+        print(self.name, "is a ", self.occup, " and he is ", self.age, "years old")
+        
+a = Person()
+b = Person()
+a.name = "Tarun"
+a.occup = "Software developer"
+
+b.name = "kumu"
+b.occup = "HR"
+
+a.info()
+b.info()
+
+
