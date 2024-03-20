50c9f62ceac0cfca14dea82cd62677e7efce679e
Tarun Reddy <132771817+Tarun6062@users.noreply.github.com>
2024-03-20 23:38:34 +0530

    Add files via upload

---
+++ 18-MODULES.py
@@ -0,0 +1,42 @@
+
+# ====================================     MODULES        ===================================================
+
+
+#  1st method
+
+"""
+import math
+x = math.sqrt(16)
+print(x)
+
+"""
+#  2nd method
+
+"""
+from math import sqrt
+x = sqrt(25)
+print(x)
+
+"""
+
+#  3rd method
+
+"""
+from math import sqrt as s 
+x = s(36)
+print(x)
+
+"""
+
+#  4th method
+
+"""
+
+from math import *
+print(sqrt(64))
+
+"""
+
+
+
+
