4430ef592ecee45888ab726d4829d77077df4c3e
Tarun Reddy <132771817+Tarun6062@users.noreply.github.com>
2024-03-27 23:29:31 +0530

    Update 11- File input&output.py\

--- 11- File input&output.py
+++ 11- File input&output.py
@@ -1,6 +1,6 @@
  # FILE I/O  
                      
-# Python can be used to perform operation on a file .( READ & Write data)   
+# Python can be used to perform operation on a file .( READ & Write data)    
            
 # TYPES OF FILES
  
